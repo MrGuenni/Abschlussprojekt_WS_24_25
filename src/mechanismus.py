4d7832b81e4451cc7b6f8163d11e183489a08e63
MrGuenni <sg1270@mci4me.at>
2025-02-03 19:08:11 +0100

    Strandbeest-Bein: Funktion eingefügt

--- src/mechanismus.py
+++ src/mechanismus.py
@@ -1,23 +1,20 @@
 class Joint:
-    #Gelenk ebener Mechanismus
     def __init__(self, x: float, y: float, fixed: bool = False):
-        self.x = x  # x-Koordinate des Gelenks
-        self.y = y  # y-Koordinate des Gelenks
-        self.fixed = fixed  # Ist das Gelenk fest oder beweglich?
+        self.x = x
+        self.y = y
+        self.fixed = fixed
 
     def __repr__(self):
         return f"Joint(x={self.x}, y={self.y}, fixed={self.fixed})"
 
 
 class Link:
-    #starre Verbindung zwischen zwei Gelenken
     def __init__(self, joint1: Joint, joint2: Joint):
         self.joint1 = joint1
         self.joint2 = joint2
         self.length = self.calculate_length()
 
     def calculate_length(self):
-        #Distanz
         return ((self.joint2.x - self.joint1.x) ** 2 + (self.joint2.y - self.joint1.y) ** 2) ** 0.5
 
     def __repr__(self):
@@ -25,7 +22,6 @@ class Link:
 
 
 class Mechanism:
-    #Hauptklasse
     def __init__(self):
         self.joints = []
         self.links = []
@@ -36,9 +32,29 @@ class Mechanism:
         return joint
 
     def add_link(self, joint1: Joint, joint2: Joint):
-        #Verbindung zwei Gelenke mit festen Glied
         link = Link(joint1, joint2)
         self.links.append(link)
 
     def __repr__(self):
         return f"Mechanism(Joints={len(self.joints)}, Links={len(self.links)})"
+
+
+def create_strandbeest_leg() -> Mechanism:
+    mech = Mechanism()
+    
+    j1 = mech.add_joint(0, 0, fixed=True)
+    j2 = mech.add_joint(5, 0)
+    j3 = mech.add_joint(4, 3)
+    j4 = mech.add_joint(1, 3, fixed=True)
+    j5 = mech.add_joint(3, 1.5)
+    j6 = mech.add_joint(2, 2)
+
+    mech.add_link(j1, j2)
+    mech.add_link(j2, j3)
+    mech.add_link(j3, j4)
+    mech.add_link(j4, j1)
+    mech.add_link(j2, j5)
+    mech.add_link(j5, j6)
+    mech.add_link(j6, j3)
+
+    return mech
