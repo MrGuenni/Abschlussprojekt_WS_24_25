e7334b1cc2893573aff2994fdae60f4a13a4886d
MrGuenni <sg1270@mci4me.at>
2025-02-03 18:00:14 +0100

    csv_v3

--- src/csv_export.py
+++ src/csv_export.py
@@ -7,8 +7,8 @@ from src.kinematik import Kinematics
 from src.mechanismus import Mechanism
 
 def export_kinematics_to_csv(kinematics: Kinematics, filename="kinematics.csv"):
-    print("CSV-Export START")
-    angles = range(0, 361, 10)
+    """Speichert die Kinematik-Daten als CSV-Datei."""
+    angles = range(0, 361, 10)  # Simuliere von 0° bis 360° in 10°-Schritten
 
     with open(filename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
