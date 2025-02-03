43c62e27e8970d32611a4ee9fd77c1d59a8df85e
MrGuenni <sg1270@mci4me.at>
2025-02-03 13:50:16 +0100

    src in visualisierung "repariert"

--- src/visualisierung.py
+++ src/visualisierung.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+
 import streamlit as st
 import matplotlib.pyplot as plt
 from src.kinematik import Kinematics
